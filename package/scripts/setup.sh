ea894ed16195ec32074f84b4628b9c2946f17264
Ali Bajwa <abajwa@hortonworks.com>
2015-04-02 11:31:34 -0700

    convert tabs to spaces

--- package/scripts/setup.sh
+++ package/scripts/setup.sh
@@ -13,19 +13,19 @@ then
     echo "Solr not found..installing"
     
     echo "adding solr user"
-	adduser solr
-	mkdir $SOLR_PATH
-	chown solr $SOLR_PATH
+    adduser solr
+    mkdir $SOLR_PATH
+    chown solr $SOLR_PATH
 
     echo "Creating HDFS dir"
-	sudo -u hdfs hdfs dfs -mkdir -p /user/solr
-	sudo -u hdfs hdfs dfs -mkdir -p /user/solr/data
+    sudo -u hdfs hdfs dfs -mkdir -p /user/solr
+    sudo -u hdfs hdfs dfs -mkdir -p /user/solr/data
 	
-	#download solr tgz and untar it
-	echo "Downloading Solr"
-	cd $SOLR_PATH
-	wget $SOLR_DOWNLOAD_LOCATION -O solr.tgz
-	tar -xvzf solr.tgz
-	ln -s solr-* latest
-	echo "Solr install complete"	
+    #download solr tgz and untar it
+    echo "Downloading Solr"
+    cd $SOLR_PATH
+    wget $SOLR_DOWNLOAD_LOCATION -O solr.tgz
+    tar -xvzf solr.tgz
+    ln -s solr-* latest
+    echo "Solr install complete"	
 fi
