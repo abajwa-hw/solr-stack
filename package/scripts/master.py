d9c56854b12b2520217b560004a006c8322e6c19
Ali Bajwa <abajwa@hortonworks.com>
2015-10-17 11:35:28 -0700

    use Ambari API to create log/pid dirs

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -42,19 +42,15 @@ class Master(Script):
             group=params.solr_group,
             content=''
     )
-             
+
+
+    if params.solr_bindir == 'UNDEFINED' or params.cloud_scripts == 'UNDEFINED':
+      Execute('Error: solr_bin: ' + params.solr_bindir + ' cloud_scripts: ' + params.cloud_scripts)
+      Execute('zxzxz')
+               
     if params.solr_downloadlocation == 'HDPSEARCH':
       Execute('yum install -y lucidworks-hdpsearch')
         
-    #Execute('echo Creating ' +  params.stack_log_dir + ' ' + status_params.stack_piddir)
-    #Execute('mkdir -p ' + params.stack_log_dir,  ignore_failures=True)
-    #Execute('mkdir -p ' + status_params.stack_piddir, ignore_failures=True)
-    #Execute('mkdir -p ' + params.solr_dir,  ignore_failures=True)    
-    #Execute('chown ' + params.solr_user + ':' + params.solr_group +  ' ' + params.stack_log_dir, ignore_failures=True)
-    #Execute('chown ' + params.solr_user + ':' + params.solr_group + ' ' + status_params.stack_piddir, ignore_failures=True)    
-    #Execute('chown ' + params.solr_user + ':' + params.solr_group + ' ' + params.solr_dir, ignore_failures=True)    
-    #Execute('touch ' + params.stack_log, owner=params.solr_user)
-    
     #form command to invoke setup.sh with its arguments and execute it
     cmd = params.service_packagedir + '/scripts/setup.sh ' + params.solr_dir + ' ' + params.solr_user + ' >> ' + params.stack_log
     Execute('echo "Running ' + cmd + '" as root')
